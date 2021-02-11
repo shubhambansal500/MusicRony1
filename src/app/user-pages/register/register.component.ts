3bc2bd73ab7a6e5139e26950fbb1a5ed69296a0c
shubham <shubhambansal500@gmail.com>
2021-02-11 21:25:16 +0530

    add Google and Facebook Api for Sign in

--- src/app/user-pages/register/register.component.ts
+++ src/app/user-pages/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { SocialAuthService } from 'angularx-social-login';
+import { SocialUser } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from 'angularx-social-login';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: SocialUser;
+  GoogleLoginProvider = GoogleLoginProvider;
+
+  constructor(private authService: SocialAuthService) { }
 
   ngOnInit() {
-    
+    this.authService.authState.subscribe(user => {
+      this.user = user;
+      if(this.user != null){
+        console.log(user.firstName)
+        console.log(user.lastName)
+        console.log(user.email)
+      }
+    });
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    console.log('------------Google Login------------------')
+    console.log(this.authService.authState)
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+    console.log('------------Facebook Login------------------')
+  }
+
+  signOut(): void {
+    if(this.user != null){
+      this.authService.signOut(true);
+      console.log('-----------------user loggedd out-------------------')
+    }
+  }
+
+  refreshGoogleToken(): void {
+    this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
   }
 
 }
