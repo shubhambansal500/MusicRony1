0f1f6af8a0c5eee08950db13e8dcc56d9f957727
mrkMadhur <madhurkulkarni95@gmail.com>
2021-03-10 01:20:46 +0530

    Analytics API

---
+++ az/analytics/index.js
@@ -0,0 +1,93 @@
+module.exports = async function (context, req) {
+    try {
+        let responseMessage = {
+            status: 200,
+            message: {}
+        };
+
+        if (!(req.query && req.query.teacher_id)) {
+            let errorMessage = "Bad Request! Please supply the following parameters:";
+            if (!req.query) throw new Error(errorMessage + " Query params");
+            if (!req.query.teacher_id) errorMessage += " teacherId";
+            throw new Error(errorMessage);
+        }
+
+        const teacher_id = req.query.teacher_id;
+        const classes = context.bindings.classes.filter(element => element.teacher_id == teacher_id);
+        let startDates = [];
+        let endDates = [];
+        let obj = {
+            revenueOverall: 0,
+            revenueCurrentMonth: 0,
+            revenueMonthlyAverage: 0,
+            anticipatedSessions: 0,
+            anticipatedSessionsCurrentMonth: 0,
+            anticipatedWorkHours: 0,
+            anticipatedWorkHoursCurrentMonth: 0,
+            totalSessionsConducted: 0
+        }
+
+        if (classes.length) {
+            classes.map((element) => {
+                let revenuePerSession = 0;
+                startDates.push(new Date(element.start_date));
+                endDates.push(new Date(element.end_date));
+                element.invitees.map((invitee) => {
+                    const activeParticipant = context.bindings.users.filter(user => {
+                        if (user.email == invitee && user.user_type == "S") {
+                            return user.teachers.find(item => {
+                                if (item.teacher_id == teacher_id && item.instrument_id == element.instrument_id && item.is_active_student == true) return true;
+                            });
+                        }
+                    }).length;
+
+                    if (activeParticipant != 0) {
+                        revenuePerSession += element.charge_per_session;
+                    }
+                });
+
+                const currentMonth = new Date().getMonth();
+                let start_date = new Date(element.start_date);
+
+                while (start_date <= new Date(element.end_date)) {
+                    if (element.recurrence.days.includes(start_date.getDay())) {
+                        obj.revenueOverall += revenuePerSession;
+                        obj.anticipatedSessions += 1;
+                        obj.anticipatedWorkHours += (element.duration_in_minutes / 60);
+
+                        if (currentMonth == start_date.getMonth()) {
+                            obj.revenueCurrentMonth += revenuePerSession;
+                            obj.anticipatedSessionsCurrentMonth += 1;
+                            obj.anticipatedWorkHoursCurrentMonth += (element.duration_in_minutes / 60);
+                        }
+
+                        if (start_date <= new Date()) {
+                            obj.totalSessionsConducted += 1;
+                        }
+                    }
+
+                    start_date.setDate(start_date.getDate() + 1);
+                }
+            });
+
+            const revenueDailyAverage = [obj.revenueOverall / ((endDates.sort()[endDates.length - 1].getTime() - startDates.sort()[0].getTime()) / (1000 * 60 * 60 * 24))];
+            obj.revenueMonthlyAverage = (revenueDailyAverage * 30).toFixed(3) + " to " + (revenueDailyAverage * 31).toFixed(3);
+            responseMessage.message = obj;
+        }
+        else {
+            responseMessage.status = 400;
+            responseMessage.message = "No matching teacherId found!"
+        }
+
+        context.res = {
+            status: responseMessage.status,
+            body: responseMessage.message
+        };
+    }
+    catch (err) {
+        context.res = {
+            status: 400,
+            body: err.message
+        };
+    }
+}
--- az/classes/index.js
+++ az/classes/index.js
@@ -7,13 +7,16 @@ module.exports = async function (context, req) {
 
         /* Validation starts */
         if (req.method == "POST") {
-            if (!(req.body && req.body.class_name && req.body.email_id && req.body.instrument_id && req.body.charge_per_session && req.body.invitees)) {
+            if (!(req.body && req.body.class_name && req.body.email_id && req.body.instrument_id && req.body.charge_per_session && req.body.invitees && req.body.policy && req.body.start_date && req.body.end_date)) {
                 let message = "Incorrect payload! Following inputs are incorrect or missing:";
                 if (!req.body) throw new Error(message + " Payload");
                 if (!req.body.class_name) message += " ClassTitle";
                 if (!req.body.email_id) message += " hostEmail";
                 if (!req.body.instrument_id) message += " instrumentId";
+                if (!req.body.start_date) message += " startDate";
+                if (!req.body.end_date) message += " endDate";
                 if (!req.body.charge_per_session) message += " chargePerSession";
+                if (!req.body.policy) message += " policy";
                 if (!req.body.invitees) message += " invitees";
                 throw new Error(message);
             }
@@ -37,12 +40,15 @@ module.exports = async function (context, req) {
         const email_id = req.method == "GET" ? req.query.email_id : req.body.email_id;
         const teacher_id = users.find(element => element.email == email_id) ? users.find(element => element.email == email_id).id : null;
         const instrument_id = req.method == "GET" ? req.query.instrument_id : req.body.instrument_id;
+        const start_date = req.body.start_date;
+        const end_date = req.body.end_date;
         const description = req.body.description;
         const duration_in_minutes = req.body.duration_in_minutes;
         const recurrence = req.body.recurrence;
         const prerequisites = req.body.prerequisites;
         const level = req.body.level;
         const charge_per_session = req.body.charge_per_session;
+        const policy = req.body.policy;
         const enable_recording = req.body.enable_recording;
         const invitees = req.body.invitees;
 
@@ -53,12 +59,15 @@ module.exports = async function (context, req) {
                     teacher_id: teacher_id,
                     email: email_id,
                     instrument_id: instrument_id,
+                    start_date: start_date,
+                    end_date: end_date,
                     description: description,
                     duration_in_minutes: duration_in_minutes,
                     recurrence: recurrence,
                     prerequisites: prerequisites,
                     level: level,
                     charge_per_session: charge_per_session,
+                    policy: policy,
                     enable_recording: enable_recording,
                     invitees: invitees
                 };
