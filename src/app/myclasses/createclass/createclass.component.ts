1de321df3cf4d3c356258187ae9b5fe4ea6568a2
shubham <shubhambansal500@gmail.com>
2021-04-02 14:28:56 +0530

    update create classes

--- src/app/myclasses/createclass/createclass.component.ts
+++ src/app/myclasses/createclass/createclass.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-createclass',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateclassComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  checkboxesInstrumentDataList = []
+  instrumentsResponse: any;
   ngOnInit(): void {
+    this.fetchInstrumentsfromAPI()
   }
 
+  async fetchInstrumentsfromAPI() {
+    await this.http.get('https://musicrony.azurewebsites.net/api/instruments?code=hWieGj0aBSLxue2eJakA4YUCVdc6ijCtJPALF10qgnNSWdNq1O0uiQ%3D%3D', { responseType: 'json' })
+      .subscribe(instruments => {
+        this.instrumentsResponse = instruments;
+        this.instrumentsResponse.forEach(data => {
+          this.checkboxesInstrumentDataList.push({
+            instrument: data.instrument,
+            id: data.id,
+            _rid: data._rid,
+            _self: data._self,
+            _etag: data._etag,
+            _attachments: data._attachments,
+            _ts: data._ts,
+            isChecked: false
+          });
+        });
+      });
+  }
 }
