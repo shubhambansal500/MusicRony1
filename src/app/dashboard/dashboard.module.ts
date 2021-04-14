9e5e00a8db1782372013c39af518e17b12a784db
shubham bansal <shubhambansal500@gmail.com>
2021-04-14 21:27:41 +0530

    create a class on dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts';
 import {InstantclassComponent} from './instantclass/instantclass.component';
+import {CreateclassComponent} from '../myclasses/createclass/createclass.component';
 
 const routes: Routes = [
   { path: 'instantclass', component: InstantclassComponent },
+  { path: 'createclass', component: CreateclassComponent },
 ]
 @NgModule({
-  declarations: [InstantclassComponent],
+  declarations: [InstantclassComponent,CreateclassComponent],
   imports: [
     FormsModule,
     CommonModule,
